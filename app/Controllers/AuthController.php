bda60b83e3f37d8e98fdf8cb84441a12797ef01b
Francis <brakay1136@gmail.com>
2026-05-10 12:15:04 -0800

    authentitcation page created for the student management system

---
+++ app/Controllers/AuthController.php
@@ -0,0 +1,25 @@
+<?php
+namespace App\Controllers;
+
+// use App\Models\Category;
+// use App\Database\Database;
+
+
+
+// require_once __DIR__ . '/../Database/Database.php';
+// require_once __DIR__ . '/../Models/Category.php';
+
+class AuthController
+{
+   
+
+   
+
+    public function index()
+    {
+         require __DIR__ . '/../views/Auth.php';
+    }
+
+
+    
+}
---
+++ app/Routes/authRoutes.php
@@ -0,0 +1,5 @@
+<?php
+return [
+    'GET /auth' => ['AuthController', 'index']
+    
+];
